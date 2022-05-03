844aa16316acd2c3a58d5e8c7e961479ceda71f0
Sudhir paladugu <16522251+spaladu@users.noreply.github.com>
2022-05-03 01:32:38 -0400

    Add files via upload

---
+++ macaddresscli.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# include shebang based on your python3 env or run> python3 filename.py --agrs MACADDRESS format while running the code.
+import requests
+import re
+import argparse
+
+# API KEY
+key = "aat_R43cnJtoaHD21AtZ65DIhS5qwhdqH"
+
+'''Below function checks the argument MACADDRESS to be valid and returns a boolean. It will accept and verifies all the below MACADDRESS formats:
+XX: XX: XX: XX: XX: XX OR  XX-XX-XX-XX-XX-XX OR XXXXXXXXXXXX'''
+
+
+def macaddress_check(mac_address):
+    check_bool = re.match(
+        "^([0-9A-Fa-f]{2}[:.-]?){5}([0-9A-Fa-f]{2})$", mac_address.strip())
+    return check_bool
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mac", type=str)
+    args = parser.parse_args()
+    mac_address = args.mac
+
+    while macaddress_check(mac_address):
+        response = requests.get(
+            "https://api.macaddress.io/v1?apiKey="+key+"&output=vendor&search="+mac_address)
+        print(response.text)
+        break
+    else:
+        print("Invalid MAC Address")
+
+
+# macaddio_request("443839ffef57")
+if __name__ == "__main__":
+    main()
